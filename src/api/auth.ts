f7a3d30c78748132533f14b10baf78503ef1fc63
hoaidanh <nguyenhoaidanh2096@gmail.com>
2019-05-13 00:08:42 +0700

    small fix, dont care this commit

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import * as MESSAGE from '@/utils/return_message';
 import {
   BoardModel, 
-  UserModel, 
+  UserModel, LogModel
 } from '@/models';
  
 const router = express.Router(); 
@@ -14,6 +14,12 @@ router.get('/me', (req, res) => {
   (async () => {
     try {
       const user = await UserModel.findOne({_id:id});
+      if(!user)res.send(
+        {
+          status: MESSAGE.ERROR
+        }
+      );
+      else
       res.send(
         {
           status: MESSAGE.QUERY_OK, user
@@ -47,6 +53,41 @@ router.post('/login', (req, res) => {
       }
     } catch (error) {
       res.send({ status: error });
+    }
+  })();
+});
+
+
+router.post('/register', (req, res) => {
+  const { username, password, role, imageUrl, email } = req.body;
+  (async () => {
+    try {
+      var existUser = await UserModel.findOne({ username });
+      var existUser1 = await UserModel.findOne({ email });
+      if (existUser !== null || existUser1 !== null)
+        res.send({
+          status: MESSAGE.USER_EXIST
+        });
+      else {
+        const u = new UserModel({ username, password, imageUrl, email });
+        var user = await u.save();
+        //add to log
+        var action = `${username} created account`;
+        const l = new LogModel({ action, ownerId: user._id });
+        await l.save();
+        //
+        user['password'] = null; //don't send pass
+        console.log(user)
+        res.send({
+          status: MESSAGE.REGISTER_OK,
+          user,
+          token: user._id
+        });
+      }
+    } catch (error) {
+      res.send({
+        status: error
+      });
     }
   })();
 });
--- src/api/user.ts
+++ src/api/user.ts
@@ -70,58 +70,7 @@ router.get('/:_id', (req, res) => {
   })();
 });
 
-router.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  (async () => {
-    try {
-     
-      const user = await UserModel.findOne({ username });
-      if (user!=null && username === user.username && password === user.password) {
-        user['password'] = null;
-        res.send({
-          status: MESSAGE.LOGIN_OK,
-          user
-        });
-      } else {
-        res.send({ status: MESSAGE.USER_INCORRECT });
-      }
-    } catch (error) {
-      res.send({ status: error });
-    }
-  })();
-});
 
-router.post('/register', (req, res) => {
-  const { username, password, role, imageUrl, email } = req.body;
-  (async () => {
-    try {
-      var existUser = await UserModel.findOne({ username });
-      var existUser1 = await UserModel.findOne({ email });
-      if (existUser !== null || existUser1 !== null)
-        res.send({
-          status: MESSAGE.USER_EXIST
-        });
-      else {
-        const u = new UserModel({ username, password, imageUrl, email });
-        var user = await u.save();
-        //add to log
-        var action = `${username} created account`;
-        const l = new LogModel({ action, ownerId: user._id });
-        await l.save();
-        //
-        user['password'] = null; //don't send pass
-        res.send({
-          status: MESSAGE.REGISTER_OK,
-          user
-        });
-      }
-    } catch (error) {
-      res.send({
-        status: error
-      });
-    }
-  })();
-});
 
 router.post('/change-pass', (req, res) => {
   const { username, password, newPassword } = req.body;
